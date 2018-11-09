92f1711d6c439cdb1229fb68b3265243b4070b5d
ksh001479 <ksh001479@daum.net>
2018-11-09 15:27:29 +0900

    웹뷰 끝 이제 옵션화면 코딩해야함

--- app/src/main/java/com/example/viewpager/Video_Day_1.java
+++ app/src/main/java/com/example/viewpager/Video_Day_1.java
@@ -3,7 +3,11 @@ package com.example.viewpager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,6 +21,7 @@ public class Video_Day_1 extends YouTubeBaseActivity {
     YouTubePlayerView youTubePlayerView;
     Button button;
     YouTubePlayer.OnInitializedListener listener;
+    WebView mWebView;
 
     Intent intent;
     int t;
@@ -28,10 +33,16 @@ public class Video_Day_1 extends YouTubeBaseActivity {
         intent = getIntent();
         t = intent.getIntExtra("int-keyword", 0);
         final String[] YouUrl = {
-                "PFMeLgqPjgE", "gKZinLW7qJ0", "PFMeLgqPjgE", "gKZinLW7qJ0", "PyDeNj-r8kQ",
+                "PFMeLgqPjgE", "CftaWmBIldU", "WmLqpNIJ0RY", "wwnF6JcRcMs", "PyDeNj-r8kQ",
                 "wQeZLHGSSyk", "mjTecsbaPPM", "CNzAY4lefxw", "VCXkO62Gnqo", "xcMk8WciHck",
-                "ReQ1nhGCUu4","cuIWjeT_SvU","F_Mt_u1Qaz4","PoDQCG2CMxU","ZU9iIbywOuM"
-                };
+                "ReQ1nhGCUu4", "cuIWjeT_SvU", "F_Mt_u1Qaz4", "PoDQCG2CMxU", "ZU9iIbywOuM"
+        };
+        String[] WebUrl = {
+                "https://ko.wikihow.com/%EC%8B%AC%EC%9E%A5-%EB%B0%9C%EC%9E%91%EC%9D%B4-%EC%9D%BC%EC%96%B4%EB%82%AC%EC%9D%84-%EB%95%8C-%EB%8C%80%EC%B2%98-%EB%B0%A9%EB%B2%95", "2", "3", "4", "5",
+                "6", "7", "8", "9", "10",
+                "11", "12", "13", "14", "15"
+        };
+
 //        TextView textView = (TextView)findViewById(R.id.text);
 //        textView.setText("전달된 값은 : "+t);
 //        getText(getInt);
@@ -54,5 +65,19 @@ public class Video_Day_1 extends YouTubeBaseActivity {
                 youTubePlayerView.initialize("AIzaSyC1jR4KIMS2UEja0fWCiszS8UJH6V0K2Hw", listener);
             }
         });
+        mWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        mWebView.loadUrl(WebUrl[t]);
+
     }
 }
